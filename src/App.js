617a310188b26205f88ff5ea9d2bd2bd770c17cf
JessicaBoos <jessica.boos28@gmail.com>
2024-10-10 17:01:37 +0200

    updated footer.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,16 @@ function App() {
     <div className="App">
       <h1>Weather App</h1>
       <WeatherSearch />
+      <p>
+        <a href="https://github.com/JessicaBoos/react-weather-app">
+          Open sourced code
+        </a>
+        , by <a href="https://github.com/JessicaBoos">Jessica Boos</a> for{" "}
+        <a href="https://www.shecodes.io/graduates/104550-jessica-boos">
+          SheCodes
+        </a>
+        .
+      </p>
     </div>
   );
 }
